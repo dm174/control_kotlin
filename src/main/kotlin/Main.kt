81eb76bb1d9a382df8319725a176e03ce3785571
Dmitry Titov <ditriy.titov@mail.ru>
2023-05-29 12:21:39 +0500

    +gitignore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package ru.netology
+fun formatTimeAgo(seconds: Long): String {
+    return when {
+        seconds in 0..60 -> "только что"
+        seconds in 61..(60 * 60) -> "${seconds / 60} минут назад"
+        seconds in (60 * 60 + 1)..(24 * 60 * 60) -> "${seconds / 3600} часов назад"
+        seconds in (24 * 60 * 60 + 1)..(2 * 24 * 60 * 60) -> "вчера"
+        seconds in (2 * 24 * 60 * 60 + 1)..(3 * 24 * 60 * 60) -> "позавчера"
+        else -> "давно"
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val time_input =130  //передать время
+    val seconds = 12345L
+
+    val timeAgo = formatTimeAgo(seconds = time_input.toLong())
+    println(timeAgo)
 }
