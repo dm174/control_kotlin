f68dddaeff2738d3bcb8975eee2b3c8b2e0395f6
Dmitry Titov <ditriy.titov@mail.ru>
2023-05-30 08:59:46 +0500

    +add minute

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,16 @@ package ru.netology
 fun formatTimeAgo(seconds: Long): String {
     return when {
         seconds in 0..60 -> "только что"
-        seconds in 61..(60 * 60) -> "${seconds / 60} минут назад"
-        seconds in (60 * 60 + 1)..(24 * 60 * 60) -> "${seconds / 3600} часов назад"
+        seconds in 61..(60 * 60) -> {
+            val minutes = (seconds / 60).toInt()
+            val minutesEnding = if (minutes % 10 == 1 && minutes % 100 != 11) "минуту" else "минуты"
+            "$minutes $minutesEnding назад"
+        }
+        seconds in (60 * 60 + 1)..(24 * 60 * 60) -> {
+            val hours = (seconds / 3600).toInt()
+            val hoursEnding = if (hours % 10 == 1 && hours % 100 != 11) "час" else "часа"
+            "$hours $hoursEnding назад"
+        }
         seconds in (24 * 60 * 60 + 1)..(2 * 24 * 60 * 60) -> "вчера"
         seconds in (2 * 24 * 60 * 60 + 1)..(3 * 24 * 60 * 60) -> "позавчера"
         else -> "давно"
@@ -11,7 +19,7 @@ fun formatTimeAgo(seconds: Long): String {
 }
 
 fun main() {
-    val time_input =130  //передать время
+    val time_input =61  //передать время
     val seconds = 12345L
 
     val timeAgo = formatTimeAgo(seconds = time_input.toLong())
